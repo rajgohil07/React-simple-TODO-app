076338be19da8c062e2d5a075656074f1e3a7e32
rajgohil07 <raj.famous007@gmail.com>
2022-06-01 10:43:47 +0530

    implemented hello world and add css to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-
-import './App.css';
-
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <h1>Hello world from react!</h1>
     </div>
   );
-}
-
-export default App;
+};
