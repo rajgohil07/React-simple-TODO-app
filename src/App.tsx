176981d9655dc66144662835faf174368febc43b
rajgohil07 <raj.famous007@gmail.com>
2022-06-01 11:03:06 +0530

    imported JSON to state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import dataJSON from "./fake-backend-data-stub/todo-data-stub.json";
+
 export const App = () => {
+  const [taskData, setTaskData] = useState(dataJSON);
+
   return (
     <div className="App">
       <h1>Hello world from react!</h1>
