5c29bb25795ec36d672c22bca38c8e65377976d0
rajgohil07 <raj.famous007@gmail.com>
2022-06-02 12:18:55 +0530

    css and html changes and added moment

--- src/component/addTask/addTask.tsx
+++ src/component/addTask/addTask.tsx
@@ -1,11 +1,19 @@
 export const AddTask = () => {
   return (
-    <form action="">
-      <label htmlFor="">Task name:</label>
-      <input type="text" name="" id="" />
-      <label htmlFor="">Date </label>
-      <input type="text" name="" id="" />
-      <input type="submit" value="Add task" />
+    <form className="add-form">
+      <div className="form-control">
+        <label htmlFor="">Task name:</label>
+        <input type="text" name="" id="" />
+      </div>
+      <div className="form-control">
+        <label htmlFor="">Select date and time</label>
+        <input type="text" name="" id="" />
+      </div>
+      <div className="form-control form-control-check">
+        <input type="checkbox" name="" id="" />
+        <label htmlFor="">is task completed?</label>
+      </div>
+      <input type="submit" className="btn btn-block" value="Add task" />
     </form>
   );
 };
